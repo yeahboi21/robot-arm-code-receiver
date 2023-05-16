61b52b2535980b71098e87f1e3bcc84dae980e3c
yeahboi21 <99054994+yeahboi21@users.noreply.github.com>
2023-05-16 09:19:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,39 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    I = receivedNumber
+})
+let V = 0
+let I = 0
+radio.setGroup(36)
+I = 99
+wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+basic.showString("R")
+basic.forever(function () {
+    if (I == 1) {
+        if (V < 90) {
+            V = V + 2
+            basic.pause(100)
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
+        }
+    } else if (I == 2) {
+        if (V > 0) {
+            V = V - 2
+            basic.pause(100)
+            wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, V)
+        }
+    }
+    if (I == 3) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 25)
+    }
+    if (I == 4) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, -35)
+    }
+    if (I == 5) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, -80)
+    }
+    if (I == 6) {
+        wuKong.setMotorSpeed(wuKong.MotorList.M2, 80)
+    }
+    if (I == 0) {
+        wuKong.stopAllMotor()
+    }
+})
